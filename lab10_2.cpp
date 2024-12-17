69ee7ed758c6e1e383faa47c79dbcfaf11cf93b5
Seacil04 <everest1756@gmail.com>
2024-12-17 16:30:58 +0700

    i wanna play game

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,10 +6,16 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+    string textline;
+	dest << "-------------------- BOOM ---------------------"<< endl;
+	while(getline(source,textline)){
+	    cout << textline <<endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
